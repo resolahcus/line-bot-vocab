b4dfe4a0a4482afbd1664f4c5d48cbe5a4622703
resolahcus <151698027+resolahcus@users.noreply.github.com>
2025-05-06 03:26:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 import os
 from flask import Flask, request
-from linebot.v3 import LineBotApi
-from linebot.v3.webhook import WebhookHandler
+from linebot import LineBotApi, WebhookHandler
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from linebot.exceptions import InvalidSignatureError
 
 app = Flask(__name__)
 
-# 使用新版的 SDK
+# 從環境變數中取得 LINE Channel 的認證
 line_bot_api = LineBotApi(os.environ.get("LINE_CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("LINE_CHANNEL_SECRET"))
 
-# 用來記錄詞彙次數
+# 用來紀錄詞彙次數的字典
 vocab_counter = {}
 
 @app.route("/callback", methods=['POST'])
 def callback():
+    # 獲取 X-Line-Signature 頭部資訊，並取得請求的 body
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
 
+    # 驗證請求的簽名
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -26,10 +27,12 @@ def callback():
 
     return 'OK'
 
+# 當接收到文字訊息時，處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text.strip()
 
+    # 處理 "!統計" 指令
     if text == "!統計":
         if not vocab_counter:
             reply = "目前沒有紀錄"
@@ -45,4 +48,5 @@ def handle_message(event):
         vocab_counter[word] = vocab_counter.get(word, 0) + 1
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # 啟動 Flask 伺服器
+    app.run()
