d60b3309507dc58a36ac49aa6d2d50369af1201f
resolahcus <151698027+resolahcus@users.noreply.github.com>
2025-05-06 15:24:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -38,9 +38,8 @@ def callback():
 def handle_message(event):
     text = event.message.text.strip()
     user_id = getattr(event.source, 'user_id', None)
-if not user_id:
-    return  # 沒 user_id 就跳過處理
-
+    if not user_id:
+        return  # 沒 user_id 就跳過處理
 
     # 嘗試取得使用者名稱
     try:
