6e9b93c21e2e4e834590e6d8f903b7cad6757f36
resolahcus <151698027+resolahcus@users.noreply.github.com>
2025-05-08 15:43:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import random
 
 app = Flask(__name__)
 
+bulls_and_cows_game = {}
+
 line_bot_api = LineBotApi(os.environ.get("LINE_CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("LINE_CHANNEL_SECRET"))
 
